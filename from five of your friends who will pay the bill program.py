92a4f0f83a01520157978109e8efc6b83156653d
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2025-08-22 06:23:55 +0530

    Add files via upload
    input name of five of your friends, and the program will tell who will pay the bill.

---
+++ from five of your friends who will pay the bill program.py
@@ -0,0 +1,9 @@
+friends=["0","1","2","3","4"]
+friends[0]=input("enter name of your first friend\n")
+friends[1]=input("enter name of your second friend\n")
+friends[2]=input("enter name of your third friend\n")
+friends[3]=input("enter name of your fourth friend\n")
+friends[4]=input("enter name of your fifth friend\n")
+import random
+fr=random.choice(friends)
+print(fr)
